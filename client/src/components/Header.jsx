8df19db8d859dce2ca66eed26174f4855251c9f2
Awofala Gbolahan <edawofal@gmail.com>
2024-10-01 19:54:01 +0100

    Updated the mobile navigation

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,7 +1,7 @@
 import { NavLink } from "react-router-dom"
 import ofriend from '../assets/ofriend.png'
-import hamburger from '../assets/hamburger.png'
-import close from '../assets/close.png'
+import hamburger from '../assets/hamburger.svg'
+import close from '../assets/cancel.svg'
 import { useState } from "react"
 
 const Header = () => {
@@ -13,7 +13,7 @@ const Header = () => {
     <nav className="flex py-3 lg:px-12 md:px-10 justify-between sm:justify-normal shadow-xl sticky top-0 bg-white z-20">
         <img src={ofriend} alt="Ofriend logo" className='mr-6 sm:px-8 px-6' />
         <button onClick={toggleMenu} className="sm:hidden pr-6">
-          <img src={isOpen ? close : hamburger} alt=""/>
+          <img src={isOpen ? close : hamburger} alt="" className="w-6"/>
         </button>
         <div className='hidden sm:flex mx-24 px-22 sm:mx-0 md:mx-24'>
           <NavLink to="/" className={({isActive}) => `mr-6 lg:ml-[150px] ${isActive ? 'text-lightblue' : 'text-black'}`}>Home</NavLink>
